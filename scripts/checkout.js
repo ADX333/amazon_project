4399113508b49618c041b6af21ab75b10f52d6fd
ADX333 <anshumandixit333@gmail.com>
2026-02-27 02:35:24 +0530

    delete button changes html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -62,7 +62,7 @@ cart.forEach((cartItem)=>{
   const standardShippingDate=formatDate(getDate(3));
   const expressShippingDate=formatDate(getDate(1));
   cartSummaryHTML+=
-  `<div class="cart-item-container">
+  `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -89,7 +89,7 @@ cart.forEach((cartItem)=>{
             Delete
           </span>
         </div>
-      </div>
+      </div> 
 
       <div class="delivery-options">
         <div class="delivery-options-title">
@@ -145,6 +145,9 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click',()=>{
    const productId=link.dataset.productId;
    removeFromCart(productId);
-   console.log(cart);
+   
+   const container=document.querySelector(`.js-cart-item-container-${productId}`)
+
+   container.remove();
 })
-});
+}); 
