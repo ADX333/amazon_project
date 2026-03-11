ae6f088ee1e10ecfe56e781aaf2f7050cec54563
ADX333 <anshumandixit333@gmail.com>
2026-03-12 01:00:28 +0530

    split checkout.js into 2 files

---
+++ scripts/checkout/orderSummary.js
@@ -0,0 +1,187 @@
+import{cart, removeFromCart} from '../../data/cart.js';
+import{products} from '../../data/products.js';
+import{formatCurrency} from '../utils/money.js';
+//import{updateCartQuantity} from 'amazon.js' ;
+
+
+export function renderOrderSummary(){
+  
+  let cartSummaryHTML='';
+
+  function getDate(daysToAdd){
+    const date=new Date();
+    date.setDate(date.getDate()+daysToAdd);
+    return date;
+  }
+
+  function formatDate(date){
+    return date.toLocaleDateString('en-US',{
+      weekday: 'long',
+      month: 'long',
+      day:'numeric'
+    });
+  }
+
+  `      <div class="payment-summary">
+            <div class="payment-summary-title">
+              Order Summary
+            </div>
+
+            <div class="payment-summary-row">
+              <div>Items (6):</div>
+              <div class="payment-summary-money">$42.75</div>
+            </div>
+
+            <div class="payment-summary-row">
+              <div>Shipping &amp; handling:</div>
+              <div class="payment-summary-money">$4.99</div>
+            </div>
+
+            <div class="payment-summary-row subtotal-row">
+              <div>Total before tax:</div>
+              <div class="payment-summary-money">$47.74</div>
+            </div>
+
+            <div class="payment-summary-row">
+              <div>Estimated tax (10%):</div>
+              <div class="payment-summary-money">$4.77</div>
+            </div>
+
+            <div class="payment-summary-row total-row">
+              <div>Order total:</div>
+              <div class="payment-summary-money">$52.51</div>
+            </div>`
+
+
+
+  cart.forEach((cartItem)=>{
+    const productId=cartItem.productId;
+    let matchingProduct;
+    products.forEach((product)=>{
+      if(product.id== productId){
+        matchingProduct=product;
+      }
+    });
+    const freeShippingDate=formatDate(getDate(7));
+    const standardShippingDate=formatDate(getDate(3));
+    const expressShippingDate=formatDate(getDate(1));
+    cartSummaryHTML+=
+    `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="delivery-date">
+        Delivery date : ${freeShippingDate}
+      </div>
+
+      <div class="cart-item-details-grid">
+        <img class="product-image"
+          src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+          <div class="product-price">
+            ₹ ${formatCurrency(matchingProduct.priceCents)}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary js-update-link">
+              Update
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
+          </div>
+        </div> 
+
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          <div class="delivery-option">
+            <input class="js-delivery-date" type="radio" checked
+              class="delivery-option-input js-delivery-option"
+              name="delivery-option-${matchingProduct.id}"
+              data-days="7"
+              >
+            <div>
+              <div class="delivery-option-date">
+                ${freeShippingDate}
+              </div>
+              <div class="delivery-option-price">
+                FREE Shipping
+              </div>
+            </div>
+          </div>
+          <div class="delivery-option">
+            <input type="radio"
+              class="delivery-option-input js-delivery-option"
+              name="delivery-option-${matchingProduct.id}"
+              data-days="3"
+              >
+            <div>
+              <div class="delivery-option-date">
+                ${standardShippingDate}
+              </div>
+              <div class="delivery-option-price">
+                ₹29 - Shipping
+              </div>
+            </div>
+          </div>
+          <div class="delivery-option">
+            <input type="radio"
+              class="delivery-option-input js-delivery-option"
+              name="delivery-option-${matchingProduct.id}"
+              data-days="1"
+              >
+            <div>
+              <div class="delivery-option-date">
+                ${expressShippingDate}
+              </div>
+              <div class="delivery-option-price">
+                ₹49 - Shipping
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  });
+
+
+  document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
+
+  document.querySelectorAll('.js-delivery-option').forEach((radio)=>{
+    radio.addEventListener('change',()=>{
+      const days=Number(radio.dataset.days);
+      const container=radio.closest('.cart-item-container');
+      const deliveryDateElement= container.querySelector('.delivery-date');
+
+      const newDate = formatDate(getDate(days));
+
+      deliveryDateElement.innerHTML=`Delivery date : ${newDate}`;
+    });
+  })
+
+  //document.querySelectorAll('.js-delivery-date').innerHTML=
+
+  document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click',()=>{
+    const productId=link.dataset.productId;
+    removeFromCart(productId);
+    renderOrderSummary();
+    
+    const container=document.querySelector(`.js-cart-item-container-${productId}`)
+
+    container.remove();
+  })
+  }); 
+  let cartQuantity=0;
+  cart.forEach((cartItem)=>{
+    cartQuantity+=cartItem.quantity;
+  });
+
+  document.querySelector('.js-return-to-home-link').innerHTML=`${cartQuantity} items`
+}
+renderOrderSummary();
