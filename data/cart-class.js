1c20a4aba7cccb12232d971a3b27fdb94c3ca7ff
ADX333 <anshumandixit333@gmail.com>
2026-03-17 03:23:53 +0530

    products.js class conversion

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,14 @@
 class Cart{
    cartItems = undefined;
-   localStorageKey=undefined;
+   #localStorageKey=undefined;
 
    constructor(localStorageKey){
-    this.localStorageKey=localStorageKey;
+    this.#localStorageKey=localStorageKey;
     this.loadFromStorage();
    }
 
    loadFromStorage() {
-    this.cartItems=JSON.parse(localStorage.getItem(this.localStorageKey));
+    this.cartItems=JSON.parse(localStorage.getItem(this.#localStorageKey));
 
     if(!this.cartItems){
       this.cartItems=[{
@@ -23,7 +23,7 @@ class Cart{
     }
     };
   saveToStorage(){
-  localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
 }
   addToCart(productId){
   let matchingItem;
@@ -68,6 +68,8 @@ class Cart{
 */
 const cart=new Cart('cart-oop');
 const businessCart= new Cart('cart-business');
+
+//cart.#localStorageKey='abcd';
 
 
 //cart.addToCart('abcd');
